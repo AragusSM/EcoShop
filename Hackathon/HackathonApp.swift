3c4eeb854c9f057afb7d59caefb7bc8dba706ac1
Ethan <eyc763@gmail.com>
2023-03-03 18:22:59 -0600

    Initial Commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Ethan Chow on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
